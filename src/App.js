d5c9d3ef39e0bfcff6fc5dec91fb31523f64abc0
dlwlsdlf <dlwlsdlf0723@naver.com>
2022-04-29 21:47:08 +0900

    #6.3 UseEfect

--- src/App.js
+++ src/App.js
@@ -3,20 +3,29 @@ import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
 function App() {
+  const [keyword, setKeyword] = useState("");
   const [counter, setValue] = useState(0);
-  const Onclick= ()=> {
-    setValue((current) => current+1 )
+  const Onclick= ()=> { setValue((current) => current+1 )};
+  const onChange = (event)=>{
+    setKeyword(event.target.value);
   };
-  console.log("i run all the time");
-  const iRunOnlyOnce = ()=>{
-    console.log("i run only once");
-  }
+  
   useEffect(()=>{
-    console.log("call the api....");
+    console.log("i run only once");
   },[]);
+
+  useEffect(()=>{
+    console.log("i run 'counter' changes ")
+  },[counter]);
+
+
+  useEffect(() => {
+    console.log("i run when 'keyword' changes")
+  },[keyword]);
 
   return (
     <div>
+      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..."/>
       <h1>{counter}</h1>
       <button onClick={Onclick}>Click me</button>
     </div>
