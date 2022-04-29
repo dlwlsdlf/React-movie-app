7370a8d22227040b36b13800a15f69af66f4654c
dlwlsdlf <dlwlsdlf0723@naver.com>
2022-04-29 21:16:20 +0900

    #6.1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const Onclick= ()=> {
+    setValue((current) => current+1 )
+  };
+  console.log("i run all the time");
+  const iRunOnlyOnce = ()=>{
+    console.log("i run only once");
+  }
+  useEffect(()=>{
+    console.log("call the api....");
+  },[]);
+
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!!</h1>
-      <Button text="Click"/>
+      <h1>{counter}</h1>
+      <button onClick={Onclick}>Click me</button>
     </div>
   );
 };
