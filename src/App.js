7c060444be068998fa93f3142adae8d5c67381e8
dlwlsdlf <dlwlsdlf0723@naver.com>
2022-04-30 21:49:42 +0900

    #7.5 Router

--- src/App.js
+++ src/App.js
@@ -1,58 +1,23 @@
-import { useState, useEffect } from "react";
+import{
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 
 
 
 function App() {
-
-  const [loding, setLoding] = useState(true);
-  const [coins, setCoins] = useState([]);
-
-  const [amount, setAmount] = useState(1);
-  const [selectCoin,setSelectCoin] = useState(1);
-
-
-  const onSelect = (eve)=>{
-    setSelectCoin(eve.target.value)
-    console.log(eve.target.value)
-  }
-  const onChange =(event)=>{
-    if(amount == ""){
-      setAmount(1)
-    }
-    setAmount(event.target.value)
-  }
-  
-  useEffect(()=>{
-    fetch("https://api.coinpaprika.com/v1/tickers")
-    .then((Response)=>Response.json())
-    .then((json) => {
-      setCoins(json)
-      setLoding(false)
-    });
-  },[]);
-
-  return (
-    <div>
-      <h1>The coins!! ({coins.length})</h1>
-      {loding ? <strong>Loding...</strong>: 
-      <select onChange={onSelect}>
-        {coins.map((coin)=>
-        <option value={coin.quotes.USD.price}>
-          {coin.name} ({coin.symbol}):${(coin.quotes.USD.price)} USD
-        </option>)}
-      </select>}
-      <div>
-      <label>Dollor : </label>
-      <input onChange={onChange} value={amount} type="number" placeholder="you haved $"/>
-      </div>
-      <div>
-      <label>you can get </label>
-      <input value={(amount/selectCoin)}/>
-      </div>
-      
-    </div>
-    );
-};
+  return( 
+  <Router>
+    <Routes>
+      <Route path="/movie" element={<Detail/>}/>
+      <Route path="/" element={<Home/>}/>
+    </Routes>
+  </Router>
+  );
+}
 
 export default App;
  
